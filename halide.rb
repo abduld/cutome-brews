0c874f1d71fdca21d9e95c68a49841510c6dbb36
Abdul Dakkak <abduld@wolfram.com>
2016-06-21 10:08:52 +0200

    update version

--- halide.rb
+++ halide.rb
@@ -2,7 +2,7 @@ class Halide < Formula
   desc "The Halide image processing language"
   homepage "http://halide-lang.org"
   url "https://github.com/halide/Halide/releases/download/release_2016_04_27/halide-mac-64-trunk-2f11b9fce62f596e832907b82d87e8f75c53dd07.tgz"
-  version "2016.03.02"
+  version "2016.04.27"
   sha256 "7304e1b8638e7529dcc38d3e7f1ac40b065cc912fa793f45ec0f1b13210d817c"
 
   def install
